faf3ecab5aada4266971c7b4d136892425ecd937
Emilyann Kerr <6244757+emilyannk@users.noreply.github.com>
2025-02-25 15:35:53 -0500

    custom query fix

--- app/services/hyrax/custom_queries/find_by_source_identifier.rb
+++ app/services/hyrax/custom_queries/find_by_source_identifier.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
-
-module Hyrax
+module Hyrax 
   module CustomQueries
-    ##
+    # @example
+    #   model = 'Publication'
+    #   deduplication_key = 'jhqsdhdwhcolh'
+    #
+    #   Hyrax.query_service.custom_queries.find_by_model_and_property_value(model:, property: :deduplication_key, value: 'jhqsdhdwhcolh')
+    #
     # @see https://github.com/samvera/valkyrie/wiki/Queries#custom-queries
+    # adapted from 
     class FindBySourceIdentifier
       def self.queries
         [:find_by_model_and_property_value]
@@ -11,40 +16,39 @@ module Hyrax
 
       def initialize(query_service:)
         @query_service = query_service
+        @connection = Hyrax.index_adapter.connection
       end
 
       attr_reader :query_service
-      delegate :resource_factory, to: :query_service
-      delegate :orm_class, to: :resource_factory
 
       ##
       # @param model [Class, #internal_resource]
-      # @param property [#to_s] the name of the property we're attempting to
-      #        query.
+      # @param property [#to_s] the solr field we're querying
       # @param value [#to_s] the propety's value that we're trying to match.
       #
       # @return [NilClass] when no record was found
       # @return [Valkyrie::Resource] when a record was found
-      #
-      # @note This is not a real estate transaction nor a Zillow lookup.
       def find_by_model_and_property_value(model:, property:, value:)
-        sql_query = sql_for_find_by_model_and_property_value
-        # NOTE: Do we need to ask the model for it's internal_resource?
-        # TODO: no => undefined method `internal_resource' for Image:Class
-        query_service.run_query(sql_query, model, property, value).first
+        @model = model
+        @property = property
+        @value = value
+
+        return if resource.blank?
+        @query_service.find_by(id: resource['id'])
       end
 
-      private
+      # Queries Solr for a document that matches the provided key
+      # @yield [Publication]
+      def resource
+        @connection.get("select", params: { q: query, fl: "*", rows: 1 })["response"]["docs"]&.first
+      end
 
-      def sql_for_find_by_model_and_property_value
-        # NOTE: This is querying the first element of the property, but we might
-        # want to check all of the elements.
-        <<-SQL
-          SELECT * FROM orm_resources
-          WHERE internal_resource = ? AND metadata -> ? ->> 0 = ?
-          LIMIT 1;
-        SQL
+      # Solr query for for a Publication with a deduplication_key_tesi that matches the provided key
+      # @return [Hash]
+      def query
+        "has_model_ssim:#{@model} && #{@property}:#{@value}"
       end
     end
   end
 end
+
